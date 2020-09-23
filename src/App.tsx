31e7b24814edad9026e1b0cf657fb71ca8f94fb8
talopl <taltal2905@gmail.com>
2020-09-23 19:17:29 +0300

    added routes from home login and signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { AuthProvider } from "./components/auth/AuthProvider/AuthProvider";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/auth/PrivateRoute/PrivateRoute";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <AuthProvider>
+      <Route>
+        <Switch>
+          <PrivateRoute path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </Route>
+    </AuthProvider>
+  );
 }
 
 export default App;
